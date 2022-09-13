e0ada31093c3eafd92503a45d971f238aee64204
Milan Kotykov <mk@allbridge.io>
2022-09-13 11:51:57 +0300

    style: prettify

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import { TokenInfo, TokenInfoEntries } from "./token-info";
 
 const DEFAULT_API_URL = "https://core-dev.a11bd.net";
 
-export async function getTokenInfo(apiUrl: string = DEFAULT_API_URL): Promise<TokenInfo> {
+export async function getTokenInfo(
+  apiUrl: string = DEFAULT_API_URL
+): Promise<TokenInfo> {
   const { data, status } = await axios.get<TokenInfoEntries>(
-      apiUrl + "/token-info",
+    apiUrl + "/token-info",
     {
       headers: {
         Accept: "application/json",
