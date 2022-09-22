464ee28bd8df38545a68ed70d49ac554c9b748ef
Anton Kozoriezov <ako@allbridge.io>
2022-09-22 13:37:04 +0300

    feat: add EVMBridge with `approve` & `send` methods prettify, eslint set up and fixes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
+import Web3 from "web3";
+import { EvmBridge } from "./chains/evm";
 import { ChainDetailsMapDTO } from "./dto/api.model";
+import { ApproveData, SendParams, TransactionResponse } from "./models";
 import { TokensInfo, mapChainDetailsMapFromDTO } from "./tokens-info";
 
 interface AllbridgeCoreSdkOptions {
@@ -23,5 +26,18 @@ export class AllbridgeCoreSdk {
       }
     );
     return new TokensInfo(mapChainDetailsMapFromDTO(data));
+  }
+
+  async evmApprove(
+    web3: Web3,
+    approveData: ApproveData
+  ): Promise<TransactionResponse> {
+    const evmBridge = new EvmBridge(web3);
+    return evmBridge.approve(approveData);
+  }
+
+  async evmSend(web3: Web3, params: SendParams): Promise<TransactionResponse> {
+    const evmBridge = new EvmBridge(web3);
+    return evmBridge.send(params);
   }
 }
