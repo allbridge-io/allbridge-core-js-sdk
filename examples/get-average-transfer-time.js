e3ba1853bb87907751e125da27981d123130e41e
Milan Kotykov <mk@allbridge.io>
2022-10-05 14:47:32 +0300

    docs: add example of using getAverageTransferTime function

---
+++ examples/get-average-transfer-time.js
@@ -0,0 +1,38 @@
+const { AllbridgeCoreSdk } = require("..");
+const configs = require("../build/src/configs");
+const { Messenger } = require("../build/cjs");
+
+function msToTime(ms) {
+  const milliseconds = ms % 1000;
+  const seconds = Math.floor((ms / 1000) % 60);
+  const minutes = Math.floor((ms / (60 * 1000)) % 60);
+  const hours = Math.floor((ms / (3600 * 1000)) % 3600);
+  return `${hours === 0 ? "" : hours + ":"}${
+    minutes < 10 ? "0" + minutes : minutes
+  }:${seconds < 10 ? "0" + seconds : seconds}.${milliseconds}`;
+}
+
+async function runExample() {
+  const sdk = new AllbridgeCoreSdk(configs.production);
+
+  const tokensInfo = await sdk.getTokensInfo();
+
+  const tokens = tokensInfo.tokens();
+  const sourceToken = tokens[0];
+  const destinationToken = tokens[tokens.length - 1];
+
+  const transferTimeMs = sdk.getAverageTransferTime(
+    sourceToken,
+    destinationToken,
+    Messenger.ALLBRIDGE
+  );
+
+  console.log(
+    "Average transfer time from %s to %s is %s",
+    sourceToken.chainSymbol,
+    destinationToken.chainSymbol,
+    msToTime(transferTimeMs)
+  );
+}
+
+runExample();
