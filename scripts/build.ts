f1a7e009707d65b0d6ce0daa58208d2710bf138a
Milan Kotykov <mk@allbridge.io>
2022-10-07 14:16:02 +0300

    fix: target browser

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,9 +2,6 @@ import path from "path";
 import { build as esbuild, BuildOptions } from "esbuild";
 
 const baseConfig: BuildOptions = {
-  platform: "node",
-  target: "esnext",
-  format: "cjs",
   nodePaths: [path.join(__dirname, "../src")],
   sourcemap: true,
   external: [],
@@ -14,14 +11,27 @@ const baseConfig: BuildOptions = {
 async function main() {
   await esbuild({
     ...baseConfig,
+    platform: "node",
+    target: "esnext",
+    format: "cjs",
     outdir: path.join(__dirname, "../build/cjs"),
     entryPoints: [path.join(__dirname, "../src/index.ts")],
   });
 
   await esbuild({
     ...baseConfig,
+    platform: "node",
+    target: "esnext",
     format: "esm",
     outdir: path.join(__dirname, "../build/esm"),
+    entryPoints: [path.join(__dirname, "../src/index.ts")],
+  });
+
+  // browser
+  await esbuild({
+    ...baseConfig,
+    format: "esm",
+    outdir: path.join(__dirname, "../build/browser"),
     entryPoints: [path.join(__dirname, "../src/index.ts")],
   });
 }
