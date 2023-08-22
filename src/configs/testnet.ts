38aa59d70c7918bbe58537c46a76fe32c2c73014
Kozer4 <ako@allbridge.io>
2023-08-22 15:09:53 +0300

    feat(solana-testnet): add sdk tx add lookUpTable

--- src/configs/testnet.ts
+++ src/configs/testnet.ts
@@ -4,7 +4,7 @@ export const testnet: AllbridgeCoreSdkOptions = {
   coreApiUrl: "https://core-dev.a11bd.net",
   coreApiHeaders: {},
   wormholeMessengerProgramId: "3u8hJUVTA4jH1wYAyUur7FFZVQ8H635K3tSHHF4ssjQ5",
-  solanaLookUpTable: "ACm9ocwiEk7DA3BBubJCmN7SwvjdrgpiaHHSy7QHaHJi",
+  solanaLookUpTable: "DPksfBY4KqcBwgxC7fUXPHEqg939z2gGpzVCoEBn4PXE",
 };
 
 export const testnetNodeUrlsDefault: NodeUrlsConfig = {
