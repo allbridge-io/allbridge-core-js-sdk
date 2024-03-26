e63c511bb202b83921cc30a89e15fcfabee75f74
jenya <jenya476@gmail.com>
2024-03-26 16:13:51 +0200

    Added BAS cctp domain

--- src/configs/mainnet.ts
+++ src/configs/mainnet.ts
@@ -1,17 +1,17 @@
-import { AllbridgeCoreSdkOptions, NodeRpcUrls, NodeUrlsConfig } from "../index";
+import { AllbridgeCoreSdkOptions, NodeRpcUrls, NodeUrlsConfig } from '../index';
 
 export const mainnet: AllbridgeCoreSdkOptions = {
-  coreApiUrl: "https://core.api.allbridgecoreapi.net",
+  coreApiUrl: 'https://core.api.allbridgecoreapi.net',
   coreApiQueryParams: {},
   coreApiHeaders: {},
-  wormholeMessengerProgramId: "worm2ZoG2kUd4vFXhvjh93UUH596ayRfgQ2MgjNMTth",
-  solanaLookUpTable: "2JcBAEVnAwVo4u8d61iqgHPrzZuugur7cVTjWubsVLHj",
-  sorobanNetworkPassphrase: "Public Global Stellar Network ; September 2015",
-  tronJsonRpc: "https://api.trongrid.io/jsonrpc",
+  wormholeMessengerProgramId: 'worm2ZoG2kUd4vFXhvjh93UUH596ayRfgQ2MgjNMTth',
+  solanaLookUpTable: '2JcBAEVnAwVo4u8d61iqgHPrzZuugur7cVTjWubsVLHj',
+  sorobanNetworkPassphrase: 'Public Global Stellar Network ; September 2015',
+  tronJsonRpc: 'https://api.trongrid.io/jsonrpc',
   cctpParams: {
-    cctpTransmitterProgramId: "CCTPmbSD7gX1bxKPAmg77w8oFzNFpaQiQUWD43TKaecd",
-    cctpTokenMessengerMinter: "CCTPiPYPc6AsJuwueEnWgSgucamXDZwBd53dQ11YiKX3",
-    cctpDomains: { ETH: 0, AVA: 1, ARB: 3, SOL: 5 },
+    cctpTransmitterProgramId: 'CCTPmbSD7gX1bxKPAmg77w8oFzNFpaQiQUWD43TKaecd',
+    cctpTokenMessengerMinter: 'CCTPiPYPc6AsJuwueEnWgSgucamXDZwBd53dQ11YiKX3',
+    cctpDomains: { ETH: 0, AVA: 1, ARB: 3, SOL: 5, BAS: 6 },
   },
 };
 
@@ -20,16 +20,16 @@ export const mainnet: AllbridgeCoreSdkOptions = {
  *  @Deprecated use {@link nodeRpcUrlsDefault}
  */
 export const nodeUrlsDefault: NodeUrlsConfig = {
-  solanaRpcUrl: "https://api.mainnet-beta.solana.com",
-  tronRpcUrl: "https://api.trongrid.io",
+  solanaRpcUrl: 'https://api.mainnet-beta.solana.com',
+  tronRpcUrl: 'https://api.trongrid.io',
 };
 
 /**
  *  This is default rpc urls for Solana and Tron,<p/> Override and use your own for proper and stable work
  */
 export const nodeRpcUrlsDefault: NodeRpcUrls = {
-  SOL: "https://api.mainnet-beta.solana.com",
-  TRX: "https://api.trongrid.io",
-  SRB: "https://rpc.stellar.org:443",
-  STLR: "https://horizon.stellar.org",
+  SOL: 'https://api.mainnet-beta.solana.com',
+  TRX: 'https://api.trongrid.io',
+  SRB: 'https://rpc.stellar.org:443',
+  STLR: 'https://horizon.stellar.org',
 };
